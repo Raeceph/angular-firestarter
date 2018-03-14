7cba6dea1f1609c19881f88e705e3336eed3b352
Raeceph <cephy88@gmail.com>
2018-03-14 15:12:43 +0800

    Environment.prod.ts changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,11 @@
-
+export const environment = {
+    production: true,
+    firebaseConfig: {
+         apiKey: "AIzaSyDmeL7c-SMV5eaZe3lNU74a8tEVTXNlueI",
+    authDomain: "anular-social-2.firebaseapp.com",
+    databaseURL: "https://anular-social-2.firebaseio.com",
+    projectId: "anular-social-2",
+    storageBucket: "",
+    messagingSenderId: "163171544710"
+    }
+};
